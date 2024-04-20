a63561818444fa1ac61a6e6283c24b907c4e403f
bethmij <bethmij@gmail.com>
2024-04-21 00:36:57 +0530

    property file added

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/CustomerController.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/CustomerController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/customerse")
+@RequestMapping("/customerr")
 public class CustomerController {
 
     @GetMapping
