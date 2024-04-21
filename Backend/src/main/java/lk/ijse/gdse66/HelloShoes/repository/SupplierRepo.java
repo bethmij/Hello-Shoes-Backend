f3c31c3481cb02892610157a0f43d1fae4b01025
bethmij <bethmij@gmail.com>
2024-04-21 13:41:00 +0530

    supplier repo added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/SupplierRepo.java
@@ -0,0 +1,7 @@
+package lk.ijse.gdse66.HelloShoes.repository;
+
+import lk.ijse.gdse66.HelloShoes.entity.Suppliers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepo extends JpaRepository<Suppliers,String> {
+}
