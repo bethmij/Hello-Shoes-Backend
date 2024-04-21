4d9289a0b6532c621bf1e070022930966aaa8a50
bethmij <bethmij@gmail.com>
2024-04-21 23:32:55 +0530

    admin dto updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/AdminPanelDTO.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/AdminPanelDTO.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.HelloShoes.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,9 +11,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class AdminPanelDTO {
 
+    @NotBlank(message = "total sales can not be null")
+    @Pattern(regexp = "-?\\d+(\\.\\d+)?", message = "total sales is not valid")
     private double totalSales;
+
+    @NotBlank(message = "total profit can not be null")
+    @Pattern(regexp = "-?\\d+(\\.\\d+)?", message = "total profit is not valid")
+
     private double totalProfit;
+
+    @NotBlank(message = "most sale item can not be null")
     private String mostSaleItem;
     private String mostSaleItemPic;
+
+    @Pattern(regexp = "\\d+", message = "most sale item qty is not valid")
+
     private int mostSaleItemQty;
 }
