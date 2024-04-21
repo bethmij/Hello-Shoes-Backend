14f15dd0a2346033bf85e85eaa7df1d33566e604
bethmij <bethmij@gmail.com>
2024-04-21 23:25:40 +0530

    Inventory entity updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Inventory.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Inventory.java
@@ -1,25 +1,58 @@
 package lk.ijse.gdse66.HelloShoes.entity;
 
+import jakarta.persistence.*;
+import lk.ijse.gdse66.HelloShoes.service.util.enums.Category;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "inventory")
 public class Inventory {
 
+    @Id
+    @Column(name = "itemCode")
     private String itemCode;
+
+    @Column(name = "item_desc", columnDefinition = "LONGTEXT")
     private String itemDesc;
+
+    @Column(name = "item_picture",columnDefinition = "LONGTEXT")
     private String itemPicture;
-    private String category;
+
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
     private int size;
-    private String supplierCode;
+
+    @ManyToOne
+    @JoinColumn(name = "supplier_code")
+    private Suppliers suppliers;
+
+    @Column(name = "supplier_name")
     private String supplierName;
+
+    @Column(name = "saling_unit_price")
     private double saleUnitPrice;
+
+    @Column(name = "buying_unit_price")
     private double buyUnitPrice;
+
+    @Column(name = "expected_profit")
     private double expectedProfit;
+
+    @Column(name = "profit_margin")
     private double profitMargin;
+
     private String status;
+
+    @OneToMany(mappedBy = "inventory", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<AdminPanel> adminPanels = new ArrayList<>();
 
 }
