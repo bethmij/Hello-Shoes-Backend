078ee23a89c357d460dc1f5f2947efe425eb2cef
bethmij <bethmij@gmail.com>
2024-04-21 23:34:15 +0530

    admin repo added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/AdminPanelRepo.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse66.HelloShoes.repository;
+
+import lk.ijse.gdse66.HelloShoes.entity.AdminPanel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdminPanelRepo extends JpaRepository<AdminPanel,Integer> {
+
+}
