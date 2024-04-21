9ef09f4847fdeb6780569db1302dcf360224386f
bethmij <bethmij@gmail.com>
2024-04-21 23:10:49 +0530

    Supplier entity updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Suppliers.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Suppliers.java
@@ -6,6 +6,9 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,21 +17,42 @@ import lombok.NoArgsConstructor;
 public class Suppliers {
 
     @Id
+    @Column(name = "supplier_code")
     private String supplierCode;
-    @Column(unique = true, nullable = false)
+
+    @Column(name = "supplier_name",unique = true, nullable = false)
     private String supplierName;
+
     @Enumerated(EnumType.STRING)
     private Category category;
+
+    @Column(name = "building")
     private String addressLine01;
+
+    @Column(name = "city_lane")
     private String addressLine02;
+
+    @Column(name = "city")
     private String addressLine03;
+
+    @Column(name = "state")
     private String addressLine04;
+
+    @Column(name = "postal_code", nullable = false)
     private String addressLine05;
-    @Column(nullable = false)
+
+    @Column(name = "country", nullable = false)
     private String addressLine06;
-    @Column(length = 15, nullable = false)
+
+    @Column(name = "contact_no",length = 15, nullable = false)
     private String contactNo1;
+
+    @Column(name = "contact_no")
     private String contactNo2;
+
     @Column(unique = true, nullable = false)
     private String email;
+
+    @OneToMany(mappedBy = "suppliers", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Inventory> inventories = new ArrayList<>();
 }
