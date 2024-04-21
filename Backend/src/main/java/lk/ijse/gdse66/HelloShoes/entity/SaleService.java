326613745686b365152c97514acf41b539c33fec
bethmij <bethmij@gmail.com>
2024-04-21 13:34:40 +0530

    sale detail added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/SaleService.java
@@ -0,0 +1,28 @@
+package lk.ijse.gdse66.HelloShoes.entity;
+
+import lk.ijse.gdse66.HelloShoes.service.util.PaymentMethod;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleService {
+
+    private String orderNo;
+    private String itemCode;
+    private String customerName;
+    private String itemDesc;
+    private int size;
+    private double unitPrice;
+    private int itemQty;
+    private double totalPrice;
+    private Timestamp purchaseDate;
+    private PaymentMethod paymentMethod;
+    private double addedPoints;
+    private String cashier;
+
+}
