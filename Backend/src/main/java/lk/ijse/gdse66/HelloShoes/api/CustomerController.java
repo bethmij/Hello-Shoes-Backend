5004e4f3d249ba23657282e006048b6ee093484a
bethmij <bethmij@gmail.com>
2024-04-25 15:29:09 +0530

    customer controller added

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/CustomerController.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/CustomerController.java
@@ -1,16 +1,49 @@
 package lk.ijse.gdse66.HelloShoes.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import lk.ijse.gdse66.HelloShoes.dto.CustomersDTO;
+import lk.ijse.gdse66.HelloShoes.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/customer")
 public class CustomerController {
 
-    @GetMapping
-    public String getMapping(){
-        System.out.println("Msg Received");
-        return "Msg sent";
+    @Autowired
+    CustomerService customerService;
+
+    @GetMapping(value ="/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<CustomersDTO> getAllCustomers(){
+        return customerService.getAllCustomers();
     }
+
+    @GetMapping(path = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public CustomersDTO getCustomerByEmail(@PathVariable("code") String code){
+        return customerService.getCustomersDetails(code);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public CustomersDTO saveCustomer( @Valid @RequestBody CustomersDTO customerDTO){
+        return customerService.saveCustomers(customerDTO);
+    }
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateCustomer(@Valid @RequestBody CustomersDTO customerDTO){
+        customerService.updateCustomers(customerDTO);
+    }
+
+    @DeleteMapping(path = "/{code}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("code") String code){
+        customerService.deleteCustomers(code);
+    }
+
+
 }
