fc4c2a4920e0bb7b6edc442ed35ca5eef800f417
bethmij <bethmij@gmail.com>
2024-04-25 23:49:17 +0530

    inventory service added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/InventoryService.java
@@ -0,0 +1,19 @@
+package lk.ijse.gdse66.HelloShoes.service;
+
+import lk.ijse.gdse66.HelloShoes.dto.InventoryDTO;
+
+import java.util.List;
+
+public interface InventoryService {
+    List<InventoryDTO> getAllInventory();
+
+    InventoryDTO getInventoryDetails(String code);
+
+    InventoryDTO saveInventory(InventoryDTO InventoryDTO);
+
+    void updateInventory(InventoryDTO InventoryDTO);
+
+    void deleteInventory(String id);
+
+//    List<String> GetSupplierCode();
+}
---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/impl/InventoryServiceImpl.java
@@ -0,0 +1,95 @@
+package lk.ijse.gdse66.HelloShoes.service.impl;
+
+import lk.ijse.gdse66.HelloShoes.dto.InventoryDTO;
+import lk.ijse.gdse66.HelloShoes.entity.Inventory;
+import lk.ijse.gdse66.HelloShoes.entity.Suppliers;
+import lk.ijse.gdse66.HelloShoes.repository.InventoryRepo;
+import lk.ijse.gdse66.HelloShoes.repository.SupplierRepo;
+import lk.ijse.gdse66.HelloShoes.service.InventoryService;
+import lk.ijse.gdse66.HelloShoes.service.exception.NotFoundException;
+import lk.ijse.gdse66.HelloShoes.service.util.GenerateID;
+import lk.ijse.gdse66.HelloShoes.service.util.Transformer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class InventoryServiceImpl implements InventoryService {
+
+    @Autowired
+    InventoryRepo inventoryRepo;
+
+    @Autowired
+    SupplierRepo supplierRepo;
+
+    @Autowired
+    Transformer transformer;
+
+    @Autowired
+    GenerateID generateID;
+
+
+    @Override
+    public List<InventoryDTO> getAllInventory() {
+        return inventoryRepo.findAll().stream()
+                .map(inventory -> transformer.fromInventoryEntity(inventory))
+                .toList();
+    }
+
+    @Override
+    public InventoryDTO getInventoryDetails(String code) {
+        if(!inventoryRepo.existsById(code)){
+            throw new NotFoundException("Item Code: " + code + " does not exist");
+        }
+
+        return transformer.fromInventoryEntity(inventoryRepo.findById(code).get());
+
+    }
+
+    @Override
+    public InventoryDTO saveInventory(InventoryDTO inventoryDTO) {
+
+        if(inventoryRepo.existsBySuppliers_SupplierCode(inventoryDTO.getSupplierCode())){
+            throw new NotFoundException("Supplier Code: " + inventoryDTO.getSupplierCode() + " exist");
+        }
+
+        inventoryDTO.setItemCode(generateID.generateItemCode());
+
+        Suppliers suppliers = supplierRepo.findBySupplierCode(inventoryDTO.getSupplierCode());
+
+        Inventory inventory = transformer.toInventoryEntity(inventoryDTO);
+        inventory.setSuppliers(suppliers);
+        inventory.setSupplierName(suppliers.getSupplierName());
+
+        return transformer.fromInventoryEntity(
+                inventoryRepo.save(inventory));
+
+
+    }
+
+    @Override
+    public void updateInventory(InventoryDTO inventoryDTO) {
+        if(!inventoryRepo.existsById(inventoryDTO.getItemCode())){
+            throw new NotFoundException("Item Code: " + inventoryDTO.getItemCode() + " does not exist");
+        }
+
+        inventoryRepo.save(transformer.toInventoryEntity(inventoryDTO));
+    }
+
+    @Override
+    public void deleteInventory(String id) {
+        if(!inventoryRepo.existsById(id)){
+            throw new NotFoundException("Item Code: " + id + " does not exist");
+        }
+        inventoryRepo.deleteById(id);
+
+    }
+
+//    @Override
+//    public List<String> GetSupplierCode() {
+//        return null;
+//    }
+}
