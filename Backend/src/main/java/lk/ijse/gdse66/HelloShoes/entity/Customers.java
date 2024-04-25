aeb141916ba0edd0dac225927c46592e0af25b7c
bethmij <bethmij@gmail.com>
2024-04-25 11:59:39 +0530

    customer dto added

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Customers.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Customers.java
@@ -1,5 +1,7 @@
 package lk.ijse.gdse66.HelloShoes.entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Gender;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Level;
 import lombok.AllArgsConstructor;
@@ -7,27 +9,59 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
-import java.sql.Timestamp;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "customers")
 public class Customers {
 
+    @Id
+    @Column(name = "customer_code")
     private String customerCode;
+
+    @Column(name = "customer_name", unique = true, nullable = false)
     private String customerName;
-    private Gender Gender;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    @Column (name = "loyalty_joined_date",columnDefinition = "DATE")
+    @JsonFormat(pattern = "yyyyMMdd")
     private Date loyaltyJoinedDate;
+
+    @Enumerated(EnumType.STRING)
     private Level level;
+
     private int totalPoints;
+
+    @Column (columnDefinition = "DATE")
+    @JsonFormat(pattern = "yyyyMMdd")
     private Date dob;
+
+    @Column(name = "building")
     private String addressLine01;
+
+    @Column(name = "city_lane")
     private String addressLine02;
+
+    @Column(name = "city", nullable = false)
     private String addressLine03;
+
+    @Column(name = "state")
     private String addressLine04;
+
+    @Column(name = "postal_code", nullable = false)
     private String addressLine05;
+
+    @Column(name = "contact_no", length = 15, nullable = false)
     private String contactNo;
+
     private String email;
-    private Timestamp recentPurchaseDate;
+
+    @Column (name = "recent_purchase_date", columnDefinition = "DATE")
+    @JsonFormat(pattern = "yyyyMMdd")
+    private Date recentPurchaseDate;
 
 }
