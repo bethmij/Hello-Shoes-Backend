e6cbd4f25f2071da983af0fe05bb94b2559b3e8e
bethmij <bethmij@gmail.com>
2024-04-25 12:47:59 +0530

    employee controller updated

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/EmployeeController.java
@@ -0,0 +1,49 @@
+package lk.ijse.gdse66.HelloShoes.api;
+
+import jakarta.validation.Valid;
+import lk.ijse.gdse66.HelloShoes.dto.EmployeeDTO;
+import lk.ijse.gdse66.HelloShoes.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    @Autowired
+    EmployeeService employeeService;
+
+    @GetMapping(value ="/getAll", produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EmployeeDTO> getAllEmployees(){
+        return employeeService.getAllEmployees();
+    }
+
+    @GetMapping(path = "/{code}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public EmployeeDTO getEmployeeByEmail(@PathVariable("code") String code){
+        return employeeService.getEmployeeDetails(code);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    public EmployeeDTO saveEmployee( @Valid @RequestBody EmployeeDTO EmployeeDTO){
+        return employeeService.saveEmployee(EmployeeDTO);
+    }
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateEmployee(@Valid @RequestBody EmployeeDTO EmployeeDTO){
+        employeeService.updateEmployee(EmployeeDTO);
+    }
+
+    @DeleteMapping(path = "/{code}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteEmployee(@PathVariable("code") String code){
+        employeeService.deleteEmployee(code);
+    }
+
+
+}
