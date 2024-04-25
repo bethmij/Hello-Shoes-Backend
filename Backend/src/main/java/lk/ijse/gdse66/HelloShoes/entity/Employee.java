296737120e11dbe4a0d2f73df15034d87a021246
bethmij <bethmij@gmail.com>
2024-04-25 10:54:43 +0530

    Employee entity updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Employee.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/Employee.java
@@ -1,7 +1,9 @@
 package lk.ijse.gdse66.HelloShoes.entity;
 
-import lk.ijse.gdse66.HelloShoes.service.util.enums.Role;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Gender;
+import lk.ijse.gdse66.HelloShoes.service.util.enums.Role;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Status;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,26 +14,71 @@ import java.sql.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Entity
+@Table(name = "employee")
 public class Employee {
 
+    @Id
+    @Column(name = "employee_code")
     private String employeeCode;
+
+    @Column(name = "employee_name", unique = true, nullable = false)
     private String employeeName;
+
+    @Column(name = "profile_pic", columnDefinition = "LONGTEXT")
     private String profilePic;
+
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+
+    @Enumerated(EnumType.STRING)
     private Status status;
+
+    @Column(nullable = false)
     private String designation;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "access_role", nullable = false)
     private Role accessRole;
+
+    @Column (columnDefinition = "DATE")
+    @JsonFormat(pattern = "yyyyMMdd")
     private Date dob;
+
+    @Column (columnDefinition = "DATE")
+    @JsonFormat(pattern = "yyyyMMdd")
     private Date dateJointed;
+
+    @Column(name = "attached_branch", nullable = false)
     private String attachedBranch;
-    private String AddressLine01;
-    private String AddressLine02;
-    private String AddressLine03;
-    private String AddressLine04;
-    private String AddressLine05;
+
+    @Column(name = "building")
+    private String addressLine01;
+
+    @Column(name = "city_lane")
+    private String addressLine02;
+
+    @Column(name = "city", nullable = false)
+    private String addressLine03;
+
+    @Column(name = "state")
+    private String addressLine04;
+
+    @Column(name = "postal_code", nullable = false)
+    private String addressLine05;
+
+    @Column(name = "contact_no", length = 15, nullable = false)
     private String contactNo;
+
+    @Column(name = "emaergency_contact", length = 10, nullable = false)
     private String emergencyContact;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
     private String guardian;
+
+    @OneToOne(mappedBy = "employee", cascade = CascadeType.ALL)
+    private User user;
 
 }
