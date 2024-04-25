d02aa723bd1d085acc91fd79a9a5c7f44bf915bf
bethmij <bethmij@gmail.com>
2024-04-25 11:37:15 +0530

    employee dto updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/EmployeeDTO.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/EmployeeDTO.java
@@ -1,5 +1,8 @@
 package lk.ijse.gdse66.HelloShoes.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Gender;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Role;
 import lk.ijse.gdse66.HelloShoes.service.util.enums.Status;
@@ -14,23 +17,45 @@ import java.sql.Date;
 @NoArgsConstructor
 public class EmployeeDTO {
 
+    @NotBlank(message = "employee code can not be null")
+    @Pattern(regexp = "EP00-[0-9]{3}", message = "employee code is not valid")
     private String employeeCode;
+
+    @NotBlank(message = "employee name can not be null")
+    @Pattern(regexp = "[A-Za-z ]+", message = "name is not valid")
     private String employeeName;
+
     private String profilePic;
     private Gender gender;
     private Status status;
+
+    @NotBlank(message = "designation can not be null")
     private String designation;
+
+    @NotNull(message = "designation code can not be null")
     private Role accessRole;
     private Date dob;
     private Date dateJointed;
+
+    @NotNull(message = "attachedBranch code can not be null")
     private String attachedBranch;
     private String AddressLine01;
     private String AddressLine02;
+
+    @NotBlank(message = "AddressLine03 code can not be null")
     private String AddressLine03;
     private String AddressLine04;
+
+    @NotBlank(message = "AddressLine05 code can not be null")
     private String AddressLine05;
+
+    @NotBlank(message = "contactNo code can not be null")
     private String contactNo;
+
+    @NotBlank(message = "emergencyContact code can not be null")
     private String emergencyContact;
+
+    @NotBlank(message = "email code can not be null")
     private String email;
     private String guardian;
 
