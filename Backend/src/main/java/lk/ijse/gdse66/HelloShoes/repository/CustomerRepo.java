48e8387c18e87015210121d08463319304fba598
bethmij <bethmij@gmail.com>
2024-04-25 15:28:59 +0530

    customer repo added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/CustomerRepo.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.HelloShoes.repository;
+
+import lk.ijse.gdse66.HelloShoes.entity.Customers;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customers,String> {
+
+    Customers findFirstByOrderByCustomerCodeDesc();
+
+//    boolean existsByEmail(String email);
+//
+//    Customers findByEmail(String email);
+}
