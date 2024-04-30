5efda7365990e3460acf6bfa88f325e0edee3b86
bethmij <bethmij@gmail.com>
2024-04-30 21:03:06 +0530

    cors error handled

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/config/CorsConfig.java
@@ -0,0 +1,16 @@
+package lk.ijse.gdse66.HelloShoes.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/app/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
