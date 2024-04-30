d17a52f7eb339caf14cf490968678f2f8f5c9d36
bethmij <bethmij@gmail.com>
2024-04-30 21:06:31 +0530

    customer dto updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/CustomersDTO.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/CustomersDTO.java
@@ -1,15 +1,10 @@
 package lk.ijse.gdse66.HelloShoes.dto;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lk.ijse.gdse66.HelloShoes.service.util.enums.Gender;
-import lk.ijse.gdse66.HelloShoes.service.util.enums.Level;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.sql.Date;
 
 @Data
 @AllArgsConstructor
@@ -23,15 +18,15 @@ public class CustomersDTO {
     @NotBlank(message = "customer name can not be null")
     @Pattern(regexp = "[A-Za-z ]+", message = "name is not valid")
     private String customerName;
-    private Gender Gender;
+//    private Gender gender;
 
-    @JsonFormat(pattern = "yyyyMMdd")
-    private Date loyaltyJoinedDate;
-    private Level level;
+//    @JsonFormat(pattern = "yyyyMMdd")
+//    private Date loyaltyJoinedDate;
+//    private Level level;
     private int totalPoints;
 
-    @JsonFormat(pattern = "yyyyMMdd")
-    private Date dob;
+//    @JsonFormat(pattern = "yyyyMMdd")
+//    private Date dob;
     private String addressLine01;
     private String addressLine02;
 
@@ -49,7 +44,7 @@ public class CustomersDTO {
     @NotBlank(message = "email code can not be null")
     private String email;
 
-    @JsonFormat(pattern = "yyyyMMdd")
-    private Date recentPurchaseDate;
+//    @JsonFormat(pattern = "yyyyMMdd")
+//    private Date recentPurchaseDate;
 
 }
