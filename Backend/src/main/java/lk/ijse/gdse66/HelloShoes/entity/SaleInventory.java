ef0d8150da7055ebafdd9b213fff4575a36668ff
bethmij <bethmij@gmail.com>
2024-05-04 01:48:33 +0530

    sale Inventory updated

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/entity/SaleInventory.java
@@ -0,0 +1,32 @@
+package lk.ijse.gdse66.HelloShoes.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "sale_inventory")
+public class SaleInventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private int size;
+
+    private double prize;
+
+    @ManyToOne
+    @JoinColumn(name = "order_no")
+    SaleServiceEntity saleService;
+
+    @ManyToOne
+    @JoinColumn(name = "item_code")
+    Inventory inventory;
+}
