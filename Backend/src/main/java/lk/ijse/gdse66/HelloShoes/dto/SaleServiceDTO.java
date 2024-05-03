2c187421fefb741aab2b15cd09fcbcb02cb119e4
bethmij <bethmij@gmail.com>
2024-05-04 01:47:52 +0530

    sale dto updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/SaleServiceDTO.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/SaleServiceDTO.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
+import java.util.Map;
 
 @Data
 @AllArgsConstructor
@@ -17,21 +18,23 @@ public class SaleServiceDTO {
     @NotBlank(message = "order no can not be null")
     @Pattern(regexp = "OR00-[0-9]{3}", message = "item code is not valid")
     private String orderNo;
-    private String itemCode;
+
+    private Map<String, Integer> inventoryList;
+ //    private String itemCode;
 
     @NotBlank(message = "customer name can not be null")
     @Pattern(regexp = "[A-Za-z ]+", message = "name is not valid")
     private String customerName;
-    private String itemDesc;
-    private int size;
+//    private String itemDesc;
+//    private int size;
 
-    @NotNull(message = "unit price can not be null")
-    @Positive(message = "unit price must be greater than zero")
-    @DecimalMin(value = "0.0", inclusive = false, message = "unit price must be greater than zero")
-    private double unitPrice;
+//    @NotNull(message = "unit price can not be null")
+//    @Positive(message = "unit price must be greater than zero")
+//    @DecimalMin(value = "0.0", inclusive = false, message = "unit price must be greater than zero")
+//    private double unitPrice;
 
-    @Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "item qty must be a valid integer")
-    private int itemQty;
+//    @Digits(integer = Integer.MAX_VALUE, fraction = 0, message = "item qty must be a valid integer")
+//    private int itemQty;
 
     @NotNull(message = "total price can not be null")
     @Positive(message = "total price must be greater than zero")
@@ -42,7 +45,7 @@ public class SaleServiceDTO {
     @NotNull(message = "purchase date can not be null")
     private Date purchaseDate;
 
-    @NotBlank(message = "payment method can not be null")
+    @NotNull(message = "payment method can not be null")
     private PaymentMethod paymentMethod;
     private double addedPoints;
 
