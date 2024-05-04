47c75e2976a2702dcf5f4c60101e33baedd65692
bethmij <bethmij@gmail.com>
2024-05-04 10:39:30 +0530

    sale inventory added

---
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/dto/SaleInventoryDTO.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse66.HelloShoes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SaleInventoryDTO {
+
+    private int id;
+
+    private String orderID;
+
+    private String itemCode;
+
+    private int size;
+
+    private double prize;
+
+}
