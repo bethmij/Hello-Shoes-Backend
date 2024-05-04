0006695ba69083f648ac795181524dbc9608a087
bethmij <bethmij@gmail.com>
2024-05-04 10:39:23 +0530

    sale inventory updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/SaleInventoryRepo.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/SaleInventoryRepo.java
@@ -3,5 +3,9 @@ package lk.ijse.gdse66.HelloShoes.repository;
 import lk.ijse.gdse66.HelloShoes.entity.SaleInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SaleInventoryRepo extends JpaRepository<SaleInventory, String> {
+
+    List<SaleInventory> findBySaleService_OrderNo(String orderNo);
 }
