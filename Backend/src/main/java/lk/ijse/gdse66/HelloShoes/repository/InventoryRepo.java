873aef5b068355a5830d3a980a32ac947eba829f
bethmij <bethmij@gmail.com>
2024-05-07 10:08:05 +0530

    inventory repo updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/InventoryRepo.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/InventoryRepo.java
@@ -2,11 +2,17 @@ package lk.ijse.gdse66.HelloShoes.repository;
 
 import lk.ijse.gdse66.HelloShoes.entity.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface InventoryRepo extends JpaRepository<Inventory,String> {
 
     Inventory findFirstByOrderByItemCodeDesc();
 
     boolean existsBySuppliers_SupplierCode(String code);
+
+    @Query("SELECT i.itemCode FROM Inventory i ORDER BY i.itemCode")
+    List<String> findAllIds();
 
 }
