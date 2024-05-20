c148db486de5e43594ff672275494c9b0444beeb
bethmij <bethmij@gmail.com>
2024-05-21 02:30:41 +0530

    admin panel updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/AdminPanelController.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/api/AdminPanelController.java
@@ -30,6 +30,41 @@ public class AdminPanelController {
         return adminPanelService.getAdminPanelByDate(Date.valueOf(date));
     }
 
+    @GetMapping("/week")
+    public AdminPanelDTO getSalesForWeek() {
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(6);
+        return adminPanelService.getProfitForWeek(startDate, endDate);
+    }
+
+    @GetMapping("/week-data")
+    public List<AdminPanelDTO> getWeekData() {
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(6);
+        return adminPanelService.getAllAdminPanelByDate(startDate, endDate);
+    }
+
+    @GetMapping("/month-data")
+    public List<AdminPanelDTO> getMonthData() {
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(30);
+        return adminPanelService.getAllAdminPanelByDate(startDate, endDate);
+    }
+
+    @GetMapping("/week-profit")
+    public Double getTotalProfitForWeek() {
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(6);
+        return adminPanelService.getTotalProfitForWeek(startDate, endDate);
+    }
+
+    @GetMapping("/week-sale")
+    public Double getTotalSaleForWeek() {
+        LocalDate endDate = LocalDate.now();
+        LocalDate startDate = endDate.minusDays(6);
+        return adminPanelService.findTotalSalesForWeek(startDate, endDate);
+    }
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public AdminPanelDTO saveAdminPanel( @Valid @RequestBody AdminPanelDTO adminPanelDTO){
--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/AdminPanelRepo.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/repository/AdminPanelRepo.java
@@ -5,7 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.Date;
+import java.util.List;
 
 public interface AdminPanelRepo extends JpaRepository<AdminPanel,Integer> {
 
@@ -14,4 +16,18 @@ public interface AdminPanelRepo extends JpaRepository<AdminPanel,Integer> {
     @Query("SELECT a FROM AdminPanel a WHERE DATE(a.date) = :date")
     AdminPanel findByDate(@Param("date") Date date);
 
+    @Query("SELECT a FROM AdminPanel a WHERE a.date BETWEEN :startDate AND :endDate ORDER BY a.totalProfit DESC LIMIT 1")
+    AdminPanel findSalesForWeek(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT SUM(a.totalProfit) FROM AdminPanel a WHERE a.date BETWEEN :startDate AND :endDate")
+    Double findTotalProfitForWeek(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT SUM(a.totalSales) FROM AdminPanel a WHERE a.date BETWEEN :startDate AND :endDate")
+    Double findTotalSalesForWeek(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+    @Query("SELECT a FROM AdminPanel a WHERE a.date BETWEEN :startDate AND :endDate ")
+    List<AdminPanel> findWeekData(@Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate);
+
+
 }
+
