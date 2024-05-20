1d19c8ef610eee9e0700ec33c8fbf129cc6e9401
bethmij <bethmij@gmail.com>
2024-05-21 02:30:33 +0530

    admin panel updated

--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/AdminPanelService.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/AdminPanelService.java
@@ -3,14 +3,23 @@ package lk.ijse.gdse66.HelloShoes.service;
 import lk.ijse.gdse66.HelloShoes.dto.AdminPanelDTO;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 public interface AdminPanelService {
     List<AdminPanelDTO> getAllAdminPanel();
 
+    List<AdminPanelDTO> getAllAdminPanelByDate(LocalDate startDate, LocalDate endDate);
+
     AdminPanelDTO getAdminPanelByDate(Date date);
 
     AdminPanelDTO saveAdminPanel(AdminPanelDTO AdminPanelDTO);
+
+    AdminPanelDTO getProfitForWeek(LocalDate startDate, LocalDate endDate);
+
+    Double getTotalProfitForWeek(LocalDate startDate, LocalDate endDate);
+
+    Double findTotalSalesForWeek(LocalDate startDate, LocalDate endDate);
 
 //    void updateAdminPanel(AdminPanelDTO AdminPanelDTO);
 //
--- Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/impl/AdminPanelServiceImpl.java
+++ Backend/src/main/java/lk/ijse/gdse66/HelloShoes/service/impl/AdminPanelServiceImpl.java
@@ -1,7 +1,10 @@
 package lk.ijse.gdse66.HelloShoes.service.impl;
 
 import lk.ijse.gdse66.HelloShoes.dto.AdminPanelDTO;
+import lk.ijse.gdse66.HelloShoes.entity.AdminPanel;
+import lk.ijse.gdse66.HelloShoes.entity.Inventory;
 import lk.ijse.gdse66.HelloShoes.repository.AdminPanelRepo;
+import lk.ijse.gdse66.HelloShoes.repository.InventoryRepo;
 import lk.ijse.gdse66.HelloShoes.service.AdminPanelService;
 import lk.ijse.gdse66.HelloShoes.service.exception.NotFoundException;
 import lk.ijse.gdse66.HelloShoes.service.util.Transformer;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.Date;
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
@@ -22,10 +26,37 @@ public class AdminPanelServiceImpl implements AdminPanelService {
     @Autowired
     Transformer transformer;
 
+    @Autowired
+    InventoryRepo inventoryRepo;
+
     @Override
     public List<AdminPanelDTO> getAllAdminPanel() {
+
         return adminPanelRepo.findAll().stream()
-                .map(adminPanel -> transformer.fromAdminPanelEntity(adminPanel))
+                .map(adminPanel -> {
+                    Inventory item = inventoryRepo.findById(adminPanel.getInventory().getItemCode()).get();
+
+                    AdminPanelDTO adminPanelDTO = transformer.fromAdminPanelEntity(adminPanel);
+                    adminPanelDTO.setMostSaleItem(item.getItemCode());
+                    adminPanelDTO.setMostSaleItemPic(item.getItemPicture());
+
+                    return adminPanelDTO;
+                })
+                .toList();
+    }
+
+    @Override
+    public List<AdminPanelDTO> getAllAdminPanelByDate(LocalDate startDate, LocalDate endDate) {
+        return adminPanelRepo.findWeekData(startDate,endDate).stream()
+                .map(adminPanel -> {
+                    Inventory item = inventoryRepo.findById(adminPanel.getInventory().getItemCode()).get();
+
+                    AdminPanelDTO adminPanelDTO = transformer.fromAdminPanelEntity(adminPanel);
+                    adminPanelDTO.setMostSaleItem(item.getItemCode());
+                    adminPanelDTO.setMostSaleItemPic(item.getItemPicture());
+
+                    return adminPanelDTO;
+                })
                 .toList();
     }
 
@@ -34,8 +65,14 @@ public class AdminPanelServiceImpl implements AdminPanelService {
         if(!adminPanelRepo.existsByDate(date)){
             throw new NotFoundException("Panel date: " + date + " does not exist");
         }
+        AdminPanel adminPanel = adminPanelRepo.findByDate(date);
+        Inventory item = inventoryRepo.findById(adminPanel.getInventory().getItemCode()).get();
 
-        return transformer.fromAdminPanelEntity(adminPanelRepo.findByDate(date));
+        AdminPanelDTO adminPanelDTO = transformer.fromAdminPanelEntity(adminPanel);
+        adminPanelDTO.setMostSaleItem(item.getItemCode());
+        adminPanelDTO.setMostSaleItemPic(item.getItemPicture());
+
+        return adminPanelDTO;
 
     }
 
@@ -45,6 +82,31 @@ public class AdminPanelServiceImpl implements AdminPanelService {
         return transformer.fromAdminPanelEntity(
                 adminPanelRepo.save(
                         transformer.toAdminPanelEntity(adminPanelDTO)));
+    }
+
+    public AdminPanelDTO getProfitForWeek(LocalDate startDate, LocalDate endDate) {
+        AdminPanel adminPanel = adminPanelRepo.findSalesForWeek(startDate, endDate);
+        Inventory item = inventoryRepo.findById(adminPanel.getInventory().getItemCode()).get();
+
+        AdminPanelDTO adminPanelDTO = transformer.fromAdminPanelEntity(adminPanel);
+        adminPanelDTO.setMostSaleItem(item.getItemCode());
+        adminPanelDTO.setMostSaleItemPic(item.getItemPicture());
+
+        return adminPanelDTO;
+
+    }
+
+    public Double getTotalProfitForWeek(LocalDate startDate, LocalDate endDate) {
+        return adminPanelRepo.findTotalProfitForWeek(startDate, endDate);
+    }
+
+    @Override
+    public Double findTotalSalesForWeek(LocalDate startDate, LocalDate endDate) {
+        return adminPanelRepo.findTotalSalesForWeek(startDate, endDate);
+    }
+
+    Double findTotalProfitForWeek(LocalDate startDate, LocalDate endDate) {
+        return adminPanelRepo.findTotalProfitForWeek(startDate, endDate);
     }
 //
 //    @Override
